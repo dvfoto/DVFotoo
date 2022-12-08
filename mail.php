45ec7255eb8f0ae5392bb4974f21c79522cb4254
Deividas <vilimaitisdeividas@gmail.com>
2022-12-08 21:57:03 +0200

    gfsgdf

--- mail.php
+++ mail.php
@@ -1,18 +1,29 @@
+
 <?php
- if(isset($_POST['submit']))
 
-$name = $_POST['name'] 
-$email = $_POST['email']
-$tel = $_POST['tel']
-$date = $_POST['date']
-$message = $_POST['message']
+// Check if form was submitted
+if (isset($_POST['submit'])) {
+    // Get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $date = $_POST['date'];
+    $message = $_POST['message'];
 
-$mailheader = "From:".$name."<".$email.">\r\n"
+    // Build email headers
+    $mailheader = "From: $name <$email>\r\n";
 
-$recipient = "dvfoto@info.lt";
+    // Send email
+    $recipient = "info@dvfoto.lt";
+    $subject = $date;
+    $success = mail($recipient, $subject, $message, $mailheader);
 
-mail($recipient, $date, $tel, $message, $mailheader)
- or die("Error!")
-echo"Message send";
+    // Check if mail was sent
+    if ($success) {
+        echo "Message sent";
+    } else {
+        echo "Error sending message";
+    }
+}
 
 ?>
