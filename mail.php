e4704aa7ae49f373623776efd8df77de9049b02d
Deividas <vilimaitisdeividas@gmail.com>
2022-12-08 21:25:14 +0200

    mail

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+ if(isset($_POST['submit']))
+
+$name = $_POST['name'] 
+$email = $_POST['email']
+$tel = $_POST['tel']
+$date = $_POST['date']
+$message = $_POST['message']
+
+$mailheader = "From:".$name."<".$email.">\r\n"
+
+$recipient = "vilimaitisdeividas@gmail.com";
+
+mail($recipient, $date, $tel, $message, $mailheader)
+ or die("Error!")
+echo"Message send";
+
+?>
