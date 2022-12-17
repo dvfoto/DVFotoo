976a4026b1d0b2d1ca91d9116f7fd011b0f2967e
Deividas <vilimaitisdeividas@gmail.com>
2022-12-17 13:43:57 +0200

    sss

--- backtoTop2.js
+++ backtoTop2.js
@@ -8,3 +8,24 @@ window.addEventListener("scroll", () => {
         toTop2.classList.remove("show");
     }
 })
+
+
+
+// Get the loading div
+var loading = document.getElementById('loading');
+
+// Show the loading div
+function showLoading() {
+    loading.style.display = 'block';
+}
+
+// Hide the loading div
+function hideLoading() {
+    loading.style.display = 'none';
+}
+
+// Listen for the content to start loading
+window.addEventListener('DOMContentLoaded', showLoading);
+
+// Listen for the content to finish loading
+window.addEventListener('load', hideLoading);
--- gallery_img.js
+++ gallery_img.js
@@ -1,4 +1,3 @@
-// Get the div element where the images will be displayed
 var imageDiv = document.getElementById("imageDiv");
 
 // Set up an array with the filenames of the images
@@ -17,14 +16,10 @@ function changeImage() {
         return;
     }
 
-    // Get the image element in the div
     const imageElement = document.querySelectorAll('img')[0];
-
-    // Update the src attribute of the image to display the new image
     imageElement.src = imageFilenames[currentIndex];
-
-    // Increment the index for the next time this function is called
     currentIndex = (currentIndex + 1) % imageFilenames.length;
+    imageDiv.classList.add("show");
 }
 
 // Function to start the image change interval
@@ -35,6 +30,7 @@ function startInterval() {
 // Function to stop the image change interval
 function stopInterval() {
     intervalRunning = false;
+    imageDiv.classList.remove("show");
 }
 
 // Call the changeImage function every 3 seconds
@@ -42,23 +38,40 @@ setInterval(changeImage, 3000);
 
 // Add event listeners to start and stop the interval when the cursor
 // is over and not over the image, respectively
-imageDiv.addEventListener("mouseover", stopInterval);
-imageDiv.addEventListener("mouseout", startInterval);
+imageDiv.addEventListener("mouseout", stopInterval);
+imageDiv.addEventListener("click", startInterval);
 
 
 
-var fs = reqnpm ruuire('fs');
 
-// The path to the folder containing the files
-const folderPath = './content/asmenines';
 
-// Read the contents of the folder
-const files = fs.readdirSync(folderPath);
 
-// Generate an array of values from the file names
-const values = files.map((fileName) => {
-    // Use the file name to generate the value
-    return generateValueFromFileName(fileName);
-});
 
-console.log(values); // Output: [value1, value2, value3, ...]
+
+
+
+
+
+
+// Add event listeners to start and stop the interval when the cursor
+// is over and not over the image, respectively
+
+
+
+
+
+// var fs = reqnpm ruuire('fs');
+
+// // The path to the folder containing the files
+// const folderPath = './content/asmenines';
+
+// // Read the contents of the folder
+// const files = fs.readdirSync(folderPath);
+
+// // Generate an array of values from the file names
+// const values = files.map((fileName) => {
+//     // Use the file name to generate the value
+//     return generateValueFromFileName(fileName);
+// });
+
+// console.log(values); // Output: [value1, value2, value3, ...]
